086e587adf964266e6c543aa78f1a038e5fc8e03
OpenACP Dev <dev@openacp.io>
2026-04-09 21:56:14 -0700

    feat: add entry points
    - src/index.ts: exports TeamsAdapter, TeamsRenderer, types, SLASH_COMMANDS
    - root index.ts: OpenACP plugin with setup/teardown lifecycle

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { TeamsAdapter } from "./adapter.js";
+export { TeamsRenderer } from "./renderer.js";
+export type { TeamsChannelConfig, TeamsPlatformData } from "./types.js";
+export type { ActivityTracker } from "./activity.js";
+export type { ToolCardSnapshot, ToolDisplaySpec, OutputMode } from "./formatting.js";
+export type { TeamsDraftManager, TeamsMessageDraft } from "./draft-manager.js";
+export type { PermissionHandler } from "./permissions.js";
+export { SLASH_COMMANDS } from "./commands/index.js";
